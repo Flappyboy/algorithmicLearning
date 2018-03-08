c4825ec309a5e95845979d3dd416cddc63ca42de
jach <jiaqi.li01@hand-china.com>
2018-03-08 09:58:23 +0800

    first

---
+++ cmake-build-debug/CMakeFiles/algorithmicLearning.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithmicLearning.dir/main.cpp.obj"
+  "algorithmicLearning.pdb"
+  "algorithmicLearning.exe"
+  "algorithmicLearning.exe.manifest"
+  "libalgorithmicLearning.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algorithmicLearning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
